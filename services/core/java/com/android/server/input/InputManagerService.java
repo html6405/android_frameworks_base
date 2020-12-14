85e54aa8c689737fe815a393ebe9c997ecac2d1b
html6405 <peter.schelchshorn@zell-net.at>
2020-12-14 19:22:04 +0100

    Revert: Forward port to toggle pointer icon when using stylus.
    Change-Id: Ie1606a1c7716613e122fd1f240fefe0e7b15f2c5 (1c973fbe)

--- services/core/java/com/android/server/input/InputManagerService.java
+++ services/core/java/com/android/server/input/InputManagerService.java
@@ -223,7 +223,6 @@ public class InputManagerService extends IInputManager.Stub
             InputChannel fromChannel, InputChannel toChannel);
     private static native void nativeSetPointerSpeed(long ptr, int speed);
     private static native void nativeSetShowTouches(long ptr, boolean enabled);
-    private static native void nativeSetStylusIconEnabled(long ptr, boolean enabled);
     private static native void nativeSetVolumeKeysRotation(long ptr, int mode);
     private static native void nativeSetInteractive(long ptr, boolean interactive);
     private static native void nativeReloadCalibration(long ptr);
@@ -351,7 +350,6 @@ public class InputManagerService extends IInputManager.Stub
         registerPointerSpeedSettingObserver();
         registerShowTouchesSettingObserver();
         registerAccessibilityLargePointerSettingObserver();
-        registerStylusIconEnabledSettingObserver();
         registerVolumeKeysRotationSettingObserver();
 
         mContext.registerReceiver(new BroadcastReceiver() {
@@ -360,7 +358,6 @@ public class InputManagerService extends IInputManager.Stub
                 updatePointerSpeedFromSettings();
                 updateShowTouchesFromSettings();
                 updateAccessibilityLargePointerFromSettings();
-                updateStylusIconEnabledFromSettings();
                 updateVolumeKeysRotationFromSettings();
             }
         }, new IntentFilter(Intent.ACTION_USER_SWITCHED), null, mHandler);
@@ -368,7 +365,6 @@ public class InputManagerService extends IInputManager.Stub
         updatePointerSpeedFromSettings();
         updateShowTouchesFromSettings();
         updateAccessibilityLargePointerFromSettings();
-        updateStylusIconEnabledFromSettings();
         updateVolumeKeysRotationFromSettings();
     }
 
@@ -1635,32 +1631,6 @@ public class InputManagerService extends IInputManager.Stub
             result = Settings.System.getIntForUser(mContext.getContentResolver(),
                     Settings.System.SHOW_TOUCHES, UserHandle.USER_CURRENT);
         } catch (SettingNotFoundException snfe) {
-        }
-        return result;
-    }
-
-    public void updateStylusIconEnabledFromSettings() {
-        int enabled = getStylusIconEnabled(0);
-        nativeSetStylusIconEnabled(mPtr, enabled != 0);
-    }
-
-    public void registerStylusIconEnabledSettingObserver() {
-        mContext.getContentResolver().registerContentObserver(
-                LineageSettings.System.getUriFor(LineageSettings.System.STYLUS_ICON_ENABLED), false,
-                new ContentObserver(mHandler) {
-                    @Override
-                    public void onChange(boolean selfChange) {
-                        updateStylusIconEnabledFromSettings();
-                    }
-                });
-    }
-
-    private int getStylusIconEnabled(int defaultValue) {
-        int result = defaultValue;
-        try {
-            result = LineageSettings.System.getInt(mContext.getContentResolver(),
-                    LineageSettings.System.STYLUS_ICON_ENABLED);
-        } catch (LineageSettings.LineageSettingNotFoundException snfe) {
         }
         return result;
     }
