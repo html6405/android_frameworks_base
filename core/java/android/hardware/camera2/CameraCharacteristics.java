a88c958e7a9f4f9f4828d60dcca85b647475bcc6
Yaroslav Zviezda <acroreiser@gmail.com>
2023-11-19 14:51:33 +0100

    Revert "Camera: Add support for extension specific metadata"
    This reverts commit 69c3bc22e77575c41e55e0fd5cab430322dce578.
    
    Change-Id: I1d402b99964a8b58a5516ec5b33c2d9ac38f2906

--- core/java/android/hardware/camera2/CameraCharacteristics.java
+++ core/java/android/hardware/camera2/CameraCharacteristics.java
@@ -4671,6 +4671,7 @@ public final class CameraCharacteristics extends CameraMetadata<CameraCharacteri
      * <p>This key is available on all devices.</p>
      * @see #SENSOR_READOUT_TIMESTAMP_NOT_SUPPORTED
      * @see #SENSOR_READOUT_TIMESTAMP_HARDWARE
+     * @hide
      */
     @PublicKey
     @NonNull
@@ -5754,8 +5755,6 @@ public final class CameraCharacteristics extends CameraMetadata<CameraCharacteri
     /*~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~
      * End generated code
      *~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~O@*/
-
-
 
 
 
