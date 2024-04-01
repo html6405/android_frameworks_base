b7729ca848084d3cb4cf9daa8a178d8b79bc3108
Yaroslav Zviezda <acroreiser@gmail.com>
2024-04-01 20:18:11 +0200

    Revert "Camera: Add concert mode extension type"
    This reverts commit 6169de0bbff8c3c05fd2e3ecd59428f0c9e363b2.

--- core/java/android/hardware/camera2/CameraExtensionCharacteristics.java
+++ core/java/android/hardware/camera2/CameraExtensionCharacteristics.java
@@ -15,7 +15,6 @@
  */
 package android.hardware.camera2;
 
-import android.annotation.FlaggedApi;
 import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
@@ -46,14 +45,10 @@ import android.os.IBinder;
 import android.os.RemoteException;
 import android.os.SystemProperties;
 import android.provider.Settings;
-import android.util.FeatureFlagUtils;
-import android.util.IntArray;
 import android.util.Log;
 import android.util.Pair;
 import android.util.Range;
 import android.util.Size;
-
-import com.android.internal.camera.flags.Flags;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -141,12 +136,6 @@ public final class CameraExtensionCharacteristics {
     public static final int EXTENSION_NIGHT = 4;
 
     /**
-     * An extension that aims to lock and stabilize a given region or object of interest.
-     */
-    @FlaggedApi(Flags.FLAG_CONCERT_MODE)
-    public static final int EXTENSION_EYES_FREE_VIDEOGRAPHY = 5;
-
-    /**
      * @hide
      */
     @Retention(RetentionPolicy.SOURCE)
@@ -154,8 +143,7 @@ public final class CameraExtensionCharacteristics {
                 EXTENSION_FACE_RETOUCH,
                 EXTENSION_BOKEH,
                 EXTENSION_HDR,
-                EXTENSION_NIGHT,
-                EXTENSION_EYES_FREE_VIDEOGRAPHY})
+                EXTENSION_NIGHT})
     public @interface Extension {
     }
 
@@ -751,18 +739,9 @@ public final class CameraExtensionCharacteristics {
         ArrayList<Integer> ret = new ArrayList<>();
         final IBinder token = new Binder(TAG + "#getSupportedExtensions:" + mCameraId);
 
-        IntArray extensionList = new IntArray(EXTENSION_LIST.length);
-        extensionList.addAll(EXTENSION_LIST);
-        if (Flags.concertMode()) {
-            extensionList.add(EXTENSION_EYES_FREE_VIDEOGRAPHY);
-        }
-
-        for (int extensionType : extensionList.toArray()) {
-            try {
-                boolean success = registerClient(mContext, token, extensionType, mCameraId,
-                        mCharacteristicsMapNative);
-                if (success && isExtensionSupported(mCameraId, extensionType,
-                        mCharacteristicsMapNative)) {
+        try {
+            for (int extensionType : EXTENSION_LIST) {
+                if (isExtensionSupported(mCameraId, extensionType, mCharacteristicsMapNative)) {
                     ret.add(extensionType);
                 }
             } finally {
