2b6b07a1b12ec110e1d0a00b0323b332d135bce1
Yaroslav Zviezda <acroreiser@gmail.com>
2024-04-01 21:21:42 +0300

    Revert "Camera: Fix OutputConfiguration equality check"
    This reverts commit 1ab5f01c94be4a9a8031f60ee86584640d566907.

--- core/java/android/hardware/camera2/params/OutputConfiguration.java
+++ core/java/android/hardware/camera2/params/OutputConfiguration.java
@@ -1402,7 +1402,6 @@ public final class OutputConfiguration implements Parcelable {
                 if (mSurfaces.get(i) != other.mSurfaces.get(i))
                     return false;
             }
-            if (!mIsDeferredConfig && mSurfaces.size() != other.mSurfaces.size()) return false;
             if (mDynamicRangeProfile != other.mDynamicRangeProfile) {
                 return false;
             }
