501c34a7fdcee7525d49ffba09fb128ac03dc328
Yaroslav Zviezda <acroreiser@gmail.com>
2024-04-01 20:16:24 +0200

    Revert "Adding JPEG_R support for Postview Images"
    This reverts commit 4e5325aa917586d984878edb4a1528484d2c56fc.

--- core/java/android/hardware/camera2/CameraExtensionCharacteristics.java
+++ core/java/android/hardware/camera2/CameraExtensionCharacteristics.java
@@ -46,6 +46,7 @@ import android.os.IBinder;
 import android.os.RemoteException;
 import android.os.SystemProperties;
 import android.provider.Settings;
+import android.util.FeatureFlagUtils;
 import android.util.IntArray;
 import android.util.Log;
 import android.util.Pair;
@@ -989,7 +990,6 @@ public final class CameraExtensionCharacteristics {
                 switch(format) {
                     case ImageFormat.YUV_420_888:
                     case ImageFormat.JPEG:
-                    case ImageFormat.JPEG_R:
                         break;
                     default:
                         throw new IllegalArgumentException("Unsupported format: " + format);
